f7ed2431edc13c820ac9504e9fdf05d8396958f3
Tamer <tamer.abousoud@gmail.com>
2025-02-05 20:35:49 -0600

    Add tracing and instrumenting

--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,23 @@
+// use env_logger::Env;
 use sqlx::PgPool;
 use std::net::TcpListener;
+
 use zero2prod::configuration::get_configuration;
 use zero2prod::startup::run;
+use zero2prod::telemetry::{get_tracing_subscriber, init_tracing_subscriber};
 
 #[tokio::main]
 async fn main() -> Result<(), std::io::Error> {
+    let tracing_subscriber = get_tracing_subscriber("zero2prod".into(), "info".into());
+    init_tracing_subscriber(tracing_subscriber);
+
     let configuration = get_configuration().expect("Failed to read configuration.");
     let connection_pool = PgPool::connect(&configuration.database.connection_string())
         .await
         .expect("Couldn't connect to Postgres.");
     let address = format!("127.0.0.1:{}", configuration.application_port);
     let listener = TcpListener::bind(address)?;
-    run(listener, connection_pool)?.await
+    run(listener, connection_pool)?.await?;
+
+    Ok(())
 }
